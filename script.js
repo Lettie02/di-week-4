8f2aa3e044acbfea221a6b87004d3b7c0b05dcc9
Lettie02 <nahmanyesther1@gmail.com>
2022-10-20 14:46:18 +0300

    ex 2

--- script.js
+++ script.js
@@ -3,19 +3,35 @@
 //   ? alert("You are in the 21st century")
 //   : alert("You live in the middle ages");
 
-var num = prompt("Enter your calcul (eg: 3 * 4)").split(" ");
-console.log(num);
-const calculator = (num1, num2, operator) => {
-  let result;
-  operator == "+"
-    ? (result = num1 + num2)
-    : operator == "-"
-    ? (result = num1 - num2)
-    : operator == "*"
-    ? (result = num1 * num2)
-    : operator == "/"
-    ? (result = num1 / num2)
-    : null;
-  alert(result);
-};
-calculator(Number(num[0]), Number(num[2]), num[1]);
+// var num = prompt("Enter your calcul (eg: 3 * 4)").split(" ");
+// console.log(num);
+// const calculator = (num1, num2, operator) => {
+//   let result;
+//   operator == "+"
+//     ? (result = num1 + num2)
+//     : operator == "-"
+//     ? (result = num1 - num2)
+//     : operator == "*"
+//     ? (result = num1 * num2)
+//     : operator == "/"
+//     ? (result = num1 / num2)
+//     : null;
+//   alert(result);
+// };
+// calculator(Number(num[0]), Number(num[2]), num[1]);
+
+// const addition = (a, b) => {
+//   console.log(a + b);
+// };
+// addition(4, 5)
+
+function makejuice(size, ingredient) {
+  size = prompt("choose your size: Small Medium or Large");
+  function addIngredients() {
+    ingredient = prompt(" choose 3 ingredients").split(" ");
+    console.log(
+      `they want a ${size} juice with ${ingredient[0]}, ${ingredient[1]} and ${ingredient[2]}`
+    );
+  }
+  addIngredients();
+}
